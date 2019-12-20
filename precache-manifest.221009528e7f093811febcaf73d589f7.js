eacac4f46f4ef0ab43be7b4cb8e1b2528dbba1ce
ksukhaldun <kseniya.khaldun@gmail.com>
2019-12-20 11:20:09 +0300

    Updates

---
+++ precache-manifest.221009528e7f093811febcaf73d589f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341e5d8cd14c538cf68ebb4a8ad63ae2",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "e00cb0664fd8762a4be3",
+    "url": "/react_dynamic-list-of-todos/static/css/main.22b03dc1.chunk.css"
+  },
+  {
+    "revision": "f2eab5a5e89191f41c53",
+    "url": "/react_dynamic-list-of-todos/static/js/2.0e7598a7.chunk.js"
+  },
+  {
+    "revision": "e00cb0664fd8762a4be3",
+    "url": "/react_dynamic-list-of-todos/static/js/main.d317b84d.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
